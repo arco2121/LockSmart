15df6fe5d3b97547a90037e1e116f1035e82c9e2
arco2121 <133692341+arco2121@users.noreply.github.com>
2024-05-03 11:35:31 +0200

    Initial

---
+++ PadLock.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LockSmart
+{
+    internal class PadLock
+    {
+        private bool locked;
+        private TinyPort motore;
+        private string code;
+        public PadLock(bool initialstate,string code,string port)
+        {
+            this.locked = initialstate;
+            this.code = code;
+            this.motore = new TinyPort(port);
+        }
+
+        public bool Locked
+        {
+            get => this.locked;
+        }
+        public void UnLock(string value)
+        {
+            if (this.locked) { }
+            else
+            {
+                if(value == this.code)
+                {
+                    this.locked = false;
+                    this.motore.objecta.Write("true");
+                }
+            }
+        }
+        public void Lock()
+        {
+            this.locked = true;
+            this.motore.objecta.Write("true");
+        }
+    }
+}
---
+++ TinyPort.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO.Ports;
+
+namespace LockSmart
+{
+    internal class TinyPort:SerialPort
+    {
+        private string port;
+        public SerialPort objecta;
+
+        public TinyPort(string port)
+        {
+            this.port = port;
+            this.objecta = new SerialPort(port,9600);
+        }
+        public void ModifyPort(string porta)
+        {
+            SerialPort port = new SerialPort(porta,9600);
+            this.objecta = port;
+        }
+        public void CheckIfHavetoClose(bool haveto)
+        {
+            if(haveto)
+            {
+                if(this.objecta.IsOpen && this.objecta != null)
+                {
+                    this.objecta.Close();
+                }
+            }
+        }
+        public string Port
+        {
+            get => this.port;
+        }
+    }
+}
