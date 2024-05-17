49b2990fe1f8c052f45b5d34688eed9b80f55277
arco2121 <colombaramarco21@gmail.com>
2024-05-17 18:11:56 +0200

    uwu

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -8,12 +9,24 @@ namespace LockSmart
 {
     internal static class Program
     {
+        static private Mutex cosino = null;
         /// <summary>
         /// Punto di ingresso principale dell'applicazione.
         /// </summary>
         [STAThread]
         static void Main()
         {
+            const string appname = "Kiwi Lock";
+            bool newone;
+            cosino = new Mutex(true, appname, out newone);
+
+            if(!newone)
+            {
+                MessageBox.Show("App già in esecuzione", "Kiwi Lock", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                Application.Exit();
+                return;
+            }
+
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Opening());
